11ffafbc5cb889e8c52ef14a030792c8884eb551
danilka32188 <60618372+danilka32188@users.noreply.github.com>
2022-01-19 05:26:58 +0500

    add config.py + fix

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения
+load_dotenv()
+
+# Реквизиты для работы с google-таблицей
+CREDENTIALS = {
+    'type': 'service_account',
+    'project_id': 'dankadimkafilms',
+    'private_key_id': os.environ.get('PRIVATE_KEY_ID'),
+    'private_key': os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
+    'client_email': 'filmbot-551@dankadimkafilms.iam.gserviceaccount.com',
+    'client_id': '114018269873706955026',
+    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+    'token_uri': 'https://oauth2.googleapis.com/token',
+    'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+    'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/filmbot-551%40dankadimkafilms.iam.gserviceaccount.com'
+}
--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
-
 from googlesheet import GoogleSheet
 from random import choice
-
-import os
-from dotenv import load_dotenv
-
-# Загружаем переменные окружения
-load_dotenv()
-
-# Реквизиты для работы с google-таблицей
-credentials = {
-    "type": "service_account",
-    "project_id": "dankadimkafilms",
-    "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
-    "private_key": os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
-    "client_email": "filmbot-551@dankadimkafilms.iam.gserviceaccount.com",
-    "client_id": "114018269873706955026",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/filmbot-551%40dankadimkafilms.iam.gserviceaccount.com"
-}
+from config import *
 
 # Объект google-таблицы
-gs = GoogleSheet(credentials)
+gs = GoogleSheet(CREDENTIALS)
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
